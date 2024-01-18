5c0cd631a3f81a3e4f279350be753ac3876bc91f
Juancho7 <juanfertrom@gmail.com>
2024-01-17 19:16:53 -0500

    componetization succesfully done

---
+++ src/data/solutions.js
@@ -0,0 +1,16 @@
+export const solutions = [
+    {
+      soltionName: 'QR Code Component',
+      linkToVisit: '/qr-code',
+      imgRoute: '/screenshots/qr-code.PNG',
+      repository:
+        'https://github.com/Juancho7/frontend-mentor-challenges-astro/blob/master/src/pages/qr-code.astro',
+    },
+    {
+      soltionName: 'Blog Preview Card',
+      linkToVisit: '/blog-preview-card',
+      imgRoute: '/screenshots/blog-preview-card.PNG',
+      repository:
+        'https://github.com/Juancho7/frontend-mentor-challenges-astro/blob/master/src/pages/blog-preview-card.astro',
+    },
+  ];
